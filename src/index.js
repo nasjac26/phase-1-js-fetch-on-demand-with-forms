a400f2be049cae482899b0c8dc79e2065ce3dec3
Jacob <Jacobonasrallah@gmail.com>
2021-09-08 20:45:30 -0700

    commit tree e89e6cb6a4a5981510d3e877f873ec50a7e9e62c

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
+
+
+
+
 const init = () => {
-  
-}
+    const inputForm = document.querySelector('form');
+    inputForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        console.log("I am an event!") 
+        console.log(event);
+        // event.target.children;
+    })}
+
 
 document.addEventListener('DOMContentLoaded', init);
